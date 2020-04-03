119c992b8aad34914705c2797d9a6c76ad902afb
denghailin <952415538@qq.com>
2020-04-03 13:49:23 +0800

    博客代码 配置依赖

--- src/main/java/com/kuailexs/kuailexs/KuailexsApplication.java
+++ src/main/java/com/kuailexs/kuailexs/KuailexsApplication.java
@@ -2,10 +2,12 @@ package com.kuailexs.kuailexs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tk.mybatis.spring.annotation.MapperScan;
 
-@MapperScan({"com.kuailexs.mirror.ubports.web.mapper","com.kuailexs.kuailexs.mapper"})
-@SpringBootApplication(scanBasePackages = {"com.kuailexs"})
+@MapperScan({"com.kuailexs.mirror.ubports.web.mapper","com.kuailexs.kuailexs.mapper","com.my.blog.website.dao"})
+@SpringBootApplication(scanBasePackages = {"com.kuailexs","com.my.blog.website"})
+@EnableTransactionManagement
 public class KuailexsApplication {
 
     public static void main(String[] args) {
